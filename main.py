b863bf75f59d8ef2ca5f4fedb64bc5e69e319480
ASUS <ASUS@DESKTOP-NB7MT4D>
2025-11-06 00:22:04 +0400

    all done

--- main.py
+++ main.py
@@ -4,64 +4,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-# area=np.array([50,60,70,80,90,100])
-# price = np.array([150000,180000,210000,220000,230000,240000])
-# #
-# # a = np.cov(area,price,bias=True)[0,1]/np.var(area)
-# # b = np.mean(price)-a*np.mean(area)
-
-# # print(a)
-# #
-# # print(b)
-# #
-# # pred= a*area+b
-# # print(pred)
-# # # plt.scatter(area,price,color='teal',label='Real Data')
-# # # plt.plot(area,price,color='red',label='Regression Liner')
-# # # plt.legend()
-# # # plt.title('Real Data')
-# # # plt.xlabel('Area')
-# # # plt.ylabel('Price')
-# # # plt.show()
-# #
-# from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
-#
-# # # mae = mean_absolute_error(price,pred)
-# # # print(mae)
-# # #
-# # # r2 = r2_score(price,pred)
-# # # print(r2)
-# #
-# #
-# from fontTools.misc.timeTools import epoch_diff
-#
-# X=area
-# Y=price
-#
-# m=0
-# b=0
-# L=0.0001
-# epochs = 3000
-# n=len(X)
-# for i in range(epochs):
-#     Y_pred = m*X + b
-#     D_m=(-2/n)*sum(X*(Y-Y_pred))
-#     D_b = (-2/n)*sum(Y-Y_pred)
-#     m=m-L*D_m
-#     b=b-L*D_b
-#
-# print(m)
-# print(b)
-#
-# mae = mean_absolute_error(Y,Y_pred)
-# mse =mean_squared_error(Y,Y_pred)
-# print(mae)
-#
-# r2 = r2_score(Y,Y_pred)
-# print(r2)
-
 df = pd.read_excel('youtube_data.xlsx')
-
 print(df.head())
 
 describe_data = df.describe()
