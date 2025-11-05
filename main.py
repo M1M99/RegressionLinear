4fd0e0b41dc7de253e87736458cf3ebec6671872
ASUS <ASUS@DESKTOP-NB7MT4D>
2025-11-06 00:19:27 +0400

    All Done

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 # area=np.array([50,60,70,80,90,100])
 # price = np.array([150000,180000,210000,220000,230000,240000])
@@ -60,40 +61,46 @@ from sklearn.linear_model import LinearRegression
 # print(r2)
 
 df = pd.read_excel('youtube_data.xlsx')
+
 print(df.head())
 
 describe_data = df.describe()
-print('CORR',df[['Likes','Comments','Views']].corr())
 print(describe_data)
 corr_matrix = df[['Likes', 'Comments', 'Views']].corr()
-
-print(corr_matrix)
-#likes
+print("CORR",corr_matrix)
+#en cox tesir eden likes
 X = df["Likes"].values
 y = df["Views"].values
-
+#likes ile view arasinda guclu elaqe var
+#views ile comment arsinda orta elaqe var
+#region Scatter
+plt.scatter(X, y)
+plt.xlabel("Likes")
+plt.ylabel("Views")
+plt.title('Likes and Views Correlation')
+plt.show()
+#endregion
 m = np.cov(X, y, bias=True)[0, 1] / np.var(X)
 b = np.mean(y) - m * np.mean(X)
 
 print("m:", m)
-print("b:", b)
+print("b: intercept", b)
 pred= m*X+b
 print(pred)
+mae =  mean_absolute_error(y, pred)
+mse =mean_squared_error(y, pred)
+r2 = r2_score(y, pred)
+print("MAE:", mae, "MSE:", mse, "R2:", r2)
 X = df[["Likes", "Comments"]]
 y = df["Views"]
 
 model = LinearRegression()
 model.fit(X, y)
-
+y_pred = model.predict(X)
+r2 = r2_score(y, y_pred)
+print('R2',r2)
 print("Coef:", model.coef_)
 print("Intercept:", model.intercept_)
-# a = np.cov(X,y,bias=True)[0,1]/np.var(X)
-# b = np.mean(y)-a*np.mean(X)
-# print(a)
-# print(b)
-# linear_regression.
-# print("COef:" ,linear_regression.coef_)
-# print("Intercept:" ,linear_regression.intercept_)
 
 
 X = df['Likes'].values
@@ -101,7 +108,8 @@ Y = df['Views'].values
 
 m = 0
 b = 0
-L = 0.0001
+# L = 0.0001 overflow
+L = 0.00000001 #normal rate
 epochs = 1000
 n = len(X)
 
@@ -127,3 +135,23 @@ plt.title('Error vs. Epochs')
 plt.xlabel('Epochs')
 plt.ylabel('Error')
 plt.show()
+
+#7. texmin et
+
+likes_count = int(input('Likes Count'))
+comments_count = int(input('Comments Count'))
+
+new_df = pd.DataFrame({
+    'Likes': [likes_count],
+    'Comments': [comments_count]
+})
+
+pred= model.predict(new_df)[0]
+print('Texmini View',pred)
+
+X_values = df[['Likes', 'Comments']]
+y_pred_all = model.predict(X_values)
+r2 = r2_score(y, y_pred_all)
+print('R2 Score:', r2)
+#multi linear daha yuksek dogrulugla isleyi(linearla muqayisede)
+#r2 score 99% ideal isleyir
